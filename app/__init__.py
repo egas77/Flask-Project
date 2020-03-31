1d809de8fdd1022d128c0a29b0d00b5a1b8774cd
Egor Koder <ebedak2003@yandex.ru>
2020-03-31 18:21:57 +0700

    Add mail

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,13 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_mail import Mail, Message
 
 from config import Develop
 
 app = Flask(__name__)
 app.config.from_object(Develop)
+
 
 db = SQLAlchemy(app)
 
@@ -14,6 +16,11 @@ from app import models
 
 migrate = Migrate(app, db)
 login_manager = LoginManager(app)
+mail = Mail(app)
+with app.app_context():
+    msg = Message("Subject", recipients=['ebedak2003@yandex.ru'])
+msg.body = 'Body message'
+mail.send(msg)
 
 
 def get_session() -> db.Session:
--- config.py
+++ config.py
@@ -6,11 +6,24 @@ class Config(object):
     RECAPTCHA_PUBLIC_KEY = '6LdyceUUAAAAAHNV2E4yIDNK3sS-vZe5KqJw4RIk'
     RECAPTCHA_PRIVATE_KEY = '6LdyceUUAAAAAARkkR8_ZIFQPaLLiFc3y9CWYvZj'
 
+    MAIL_SERVER = 'smtp.yandex.ru'
+    MAIL_PORT = 465
+    MAIL_USE_SSL = True
+    MAIL_USERNAME = 'flask-blog@yandex.ru'
+    MAIL_DEFAULT_SENDER = 'flask-blog@yandex.ru'
+    MAIL_PASSWORD = 'av8-JJm-JFY-jiS'
 
-class Develop(Config):
+
+class Testing(Config):
     TESTING = True
     DEBUG = True
 
 
+class Develop(Config):
+    TESTING = False
+    DEBUG = True
+
+
 class Production(Config):
+    TESTING = False
     DEBUG = False
