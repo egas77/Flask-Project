b0c25d689279b81d30bb35bfbda1a93a85fff94b
Egor Koder <ebedak2003@yandex.ru>
2020-04-09 00:35:09 +0700

    Add function create post

--- app/blueprints/post.py
+++ app/blueprints/post.py
@@ -4,28 +4,41 @@ from flask_login import login_required
 from app import get_session
 from app.models import Post
 
+import datetime
+
 blueprint_post = Blueprint('post', __name__, template_folder='templates')
 
 
-@blueprint_post.route('/new-post', methods=['GET', 'POST'])
+@blueprint_post.route('/create-post', methods=['GET', 'POST'])
+@blueprint_post.route('/edit-post/<int:post_id>', methods=['GET', 'POST'])
 @login_required
-def new_post():
+def create_post(post_id=None):
     if request.method == 'POST':
-        post = Post()
+        if post_id:
+            post = Post.get_query().get(post_id)
+        else:
+            post = Post()
+            post.publication_date = datetime.datetime.now()
         title = request.form.get('title-post')
         content = request.form.get('content', None)
         post.title = title
         post.content = content
         session = get_session()
-        session.add(post)
+        if not post_id:
+            session.add(post)
         session.commit()
         return redirect(url_for('index'))
+    if post_id:
+        post = Post.get_query().get(post_id)
+        return render_template('new_post.html', post=post)
     return render_template('new_post.html')
 
 
 @blueprint_post.route('/post/<int:post_id>')
 def view_post(post_id):
-    return render_template('index.html')
+    print(post_id)
+    post = Post.get_query().get(post_id)
+    return render_template('post.html', post=post)
 
 
 @blueprint_post.route('/upload-image', methods=['POST'])
