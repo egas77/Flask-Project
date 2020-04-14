1daa3d6fd53ec1e343d0146464581a4f06668026
Egor Koder <ebedak2003@yandex.ru>
2020-04-14 19:29:50 +0700

    Update config

--- config.py
+++ config.py
@@ -1,22 +1,22 @@
 class Config(object):
     # App settings
-    SECRET_KEY = None
-    SECURITY_PASSWORD_SALT = None
+    SECRET_KEY = 'LMTk06lLwpD4HlZl'
+    SECURITY_PASSWORD_SALT = 'iZRMbSOFBYu4xoOW'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///data\\db\\db.sqlite'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     FLASK_APP = 'main.py'
-    RECAPTCHA_PUBLIC_KEY = None
-    RECAPTCHA_PRIVATE_KEY = None
+    RECAPTCHA_PUBLIC_KEY = '6LfcguUUAAAAAMWvFiu8rpazGR61ZxuIDbtkaG65'
+    RECAPTCHA_PRIVATE_KEY = '6LfcguUUAAAAAEWzpN78kxcUj31kU8FAuQn2UG8S'
 
     # Mail settings
-    MAIL_SERVER = None
+    MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 465
     MAIL_USE_SSL = True
     MAIL_USE_TLS = False
-    MAIL_USERNAME = None
-    MAIL_DEFAULT_SENDER = None
-    MAIL_PASSWORD = None
-    FEEDBACK_MAIL = None
+    MAIL_USERNAME = 'blogflask89@gmail.com'
+    MAIL_DEFAULT_SENDER = 'blogflask89@gmail.com'
+    MAIL_PASSWORD = 'av8-JJm-JFY-jiS'
+    FEEDBACK_MAIL = 'ebedak2003@yandex.ru'
 
     # User settings
     POSTS_ON_PAGE = 5
